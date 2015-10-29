7b0f4e94ea4e9771b8d9e370ae7430250a911ef2
Sascha Schimke <sascha@schimke.me>
2015-10-29 21:31:33 +0100

    Basis assertions (is_integer, is_resource, is_scalar, ...)

--- lib/BaseAssertionTrait.php
+++ lib/BaseAssertionTrait.php
@@ -41,4 +41,76 @@ trait BaseAssertionTrait
 
         return $this;
     }
+
+    public function isInteger()
+    {
+        $this->doCheck(function ($value) {
+            $this->throwExceptionIfFalse(
+                is_integer($value),
+                'Failed assertion that %s is integer', $value
+            );
+        });
+
+        return $this;
+    }
+
+    public function isFloat()
+    {
+        $this->doCheck(function ($value) {
+            $this->throwExceptionIfFalse(
+                is_float($value),
+                'Failed assertion that %s is float', $value
+            );
+        });
+
+        return $this;
+    }
+
+    public function isNumeric()
+    {
+        $this->doCheck(function ($value) {
+            $this->throwExceptionIfFalse(
+                is_numeric($value),
+                'Failed assertion that %s is numeric', $value
+            );
+        });
+
+        return $this;
+    }
+
+    public function isScalar()
+    {
+        $this->doCheck(function ($value) {
+            $this->throwExceptionIfFalse(
+                is_scalar($value),
+                'Failed assertion that %s is scalar', $value
+            );
+        });
+
+        return $this;
+    }
+
+    public function isResource()
+    {
+        $this->doCheck(function ($value) {
+            $this->throwExceptionIfFalse(
+                is_resource($value),
+                'Failed assertion that %s is a resource', $value
+            );
+        });
+
+        return $this;
+    }
+
+    public function isTrue()
+    {
+        $this->doCheck(function ($value) {
+            $this->throwExceptionIfFalse(
+                true === $value,
+                'Failed assertion that %s is true', $value
+            );
+        });
+
+        return $this;
+    }
 }
